bfe53d1b5ca44b2ad9c332521d8967860e6585cb
Joseth David <davidguerrero777@gmail.com>
2014-04-21 15:01:49 -0500

    Con error newMateria
    Creamos el formulario para agregar materia, pero Django rechaza la
    petición.

--- scripts/MateriasCtrl.js
+++ scripts/MateriasCtrl.js
@@ -1,10 +1,17 @@
 angular.module('rutasApp')
 .controller('MateriasCtrl', ['$scope', '$http', function ($scope, $http) {
-	$scope.Materias = [
-		{nombre: 'Informatica', grupo: '10'},
-		{nombre: 'Matematicas', grupo: '10'},
-		{nombre: 'Español', grupo: '11'},
-		{nombre: 'Inglés', grupo: '11'},
-		{nombre: 'Informatica', grupo: '9'}
-	];
+	$http.get('http://localhost:8000/materias/')
+	.success(function(datos){
+		$scope.Materias = datos;
+	});
+
+	$scope.mateNueva = [];
+
+	$scope.saveNewMateria = function(){
+		$http.post('http://localhost:8000/materias/', $.param( $scope.mateNueva))
+		.success(function(r){
+			console.log('Materia guardada.');
+			console.log(r);
+		});
+	}
 }])
